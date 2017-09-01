a5bb81384c40b4c3cb7582ddd3b2de513e92f5ad
matsrietdijk <matsrietdijk@gmail.com>
2017-09-01 19:02:42 +0200

    Format code

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,17 @@
-type Player =
-    Cross | Circle
+module Main exposing (..)
+
+
+type Player
+    = Cross
+    | Circle
 
 
 type alias Tile =
     Maybe Player
 
 
-type Board =
-    Board Tile Tile Tile Tile Tile Tile Tile Tile Tile
+type Board
+    = Board Tile Tile Tile Tile Tile Tile Tile Tile Tile
 
 
 type alias Model =
@@ -30,18 +34,27 @@ init =
 winner : Board -> Maybe Player
 winner board =
     let
-        checkTiles a b c = if (a == b && b == c) then Just a else Nothing
-    in
-        case board of
-            Board (Just a) (Just b) (Just c) _ _ _ _ _ _ ->
-                checkTiles a b c
-            Board _ _ _ (Just a) (Just b) (Just c) _ _ _ ->
-                checkTiles a b c
-            Board _ _ _ _ _ _ (Just a) (Just b) (Just c) ->
-                checkTiles a b c
-            Board (Just a) _ _ _ (Just b) _ _ _ (Just c) ->
-                checkTiles a b c
-            Board _ _ (Just a) _ (Just b) _ (Just c) _ _ ->
-                checkTiles a b c
-            _ ->
+        checkTiles a b c =
+            if a == b && b == c then
+                Just a
+            else
                 Nothing
+    in
+    case board of
+        Board (Just a) (Just b) (Just c) _ _ _ _ _ _ ->
+            checkTiles a b c
+
+        Board _ _ _ (Just a) (Just b) (Just c) _ _ _ ->
+            checkTiles a b c
+
+        Board _ _ _ _ _ _ (Just a) (Just b) (Just c) ->
+            checkTiles a b c
+
+        Board (Just a) _ _ _ (Just b) _ _ _ (Just c) ->
+            checkTiles a b c
+
+        Board _ _ (Just a) _ (Just b) _ (Just c) _ _ ->
+            checkTiles a b c
+
+        _ ->
+            Nothing
